85a362e2cb3e1dfa89ff271512b2dddbcb5c7874
Mohan Ashapu <ashapumohan123@gmail.com>
2025-06-13 21:06:34 +0530

    Update Certificates.js

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -116,7 +116,7 @@ const Certificates = () => {
 
     return (
         <div className=" bg-gradient-to-tr from-blue-50 to-indigo-50 min-h-screen p-4 sm:p-6 my-15">
-            <h1 className="text-3xl font-bold text-center ">Certificates</h1>
+            <h1 className="text-3xl font-bold text-center mb-10">Certificates</h1>
 
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
